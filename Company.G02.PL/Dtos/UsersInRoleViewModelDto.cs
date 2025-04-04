dc637a2644a9c1685ef06e594910e1d914217a82
Mohamedelsayed219 <melsayed299@yahoo.com>
2025-04-04 19:02:45 +0200

    implementation Roles

---
+++ Company.G02.PL/Dtos/UsersInRoleViewModelDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Company.G02.PL.Dtos
+{
+    public class UsersInRoleViewModelDto
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
